cabb66ec98f7200f8eaef274627a80d364855ac4
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-06 01:08:41 -0300

    primeiro parametro nas views é request, n response

--- tech with tim/django_tutorial/meu_projeto/principal/views.py
+++ tech with tim/django_tutorial/meu_projeto/principal/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 
 # Create your views here.
-def index(response, id):  # não seria request? 
+def index(request, id):
     ls = ToDoList.objects.get(id=id)
     item = ls.item_set.get(id=1)
     return HttpResponse(f'<h1> {ls.name} </h1> <br> <h2> {item.text} </h2>')
